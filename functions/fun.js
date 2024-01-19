e7313ec24f24fb5c3ed819e7fa8420e917904a6e
Samridh Gupta <samridhgupta1053@gmail.com>
2024-01-20 02:10:15 +0530

    [samridh] updated: functions

---
+++ functions/fun.js
@@ -0,0 +1,27 @@
+
+/*
+function doSomething(callback) {
+    
+    callback();
+  }
+  
+  doSomething(function() {
+    console.log("Callback executed");
+  });
+*/
+
+/*
+  function multiply(a, b) {
+    return a * b;
+  }
+  */
+
+  /*
+  const add = function (a, b) {
+    return a + b;
+  };
+  */
+
+  (function() {
+    console.log("I am not talented but I am hard-working person!");
+  })();
