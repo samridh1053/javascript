cdeb0910cdf9f9fc1a34dec0da817aa7499e6996
Samridh Gupta <samridhgupta1053@gmail.com>
2024-01-20 00:57:25 +0530

    [sasmridh] added: new functions

--- array-operation/arr.js
+++ array-operation/arr.js
@@ -1,7 +1,34 @@
+/*
 let arr1 =[10, 20, 30];
 let arr2 =[40, 50, 60];
 let arr3 = [...arr1,...arr2];
 console.log(arr3);
+*/
+
+/*
+let numbers = [1, 2, 3, 4, 5];
+
+let evenNumbers = numbers.filter(function(number) {
+  return number % 2 === 0;
+});
+
+console.log(evenNumbers);
+*/
+//         pop function   //
+
+/*
+let myArray = [1, 2, 3, 4];
+let removedElement = myArray.pop();
+console.log(removedElement)
+*/
+
+//         push function             //
+let myArray = [1, 2, 3];
+myArray.push(4);
+console.log(myArray);
+
+
+
 
 
 
