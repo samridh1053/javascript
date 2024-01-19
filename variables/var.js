2ba8007d540665dfc091f6eed1a57b086f13d133
Samridh Gupta <samridhgupta1053@gmail.com>
2024-01-20 01:57:13 +0530

    [samridh] updated: var,let,const

---
+++ variables/var.js
@@ -0,0 +1,41 @@
+/*
+function foo(){
+    var n=6;
+    console.log(n);
+ }
+ 
+ var n=5;
+ console.log(n);
+ foo();
+ */
+
+ /*
+ var myVar=10;
+console.log(myVar)
+var myVar=15;
+console.log(myVar)
+*/
+
+//   let function //
+/*
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(z)
+*/
+
+/*
+let sum =5+1;
+let product =2*4;
+console.log(sum)
+console.log(product)
+*/
+
+// const //
+const colors = ['red', 'green', 'blue'];
+console.log(colors[0]);  
+
+colors.push('yellow'); 
+console.log(colors.length);  
+
+
